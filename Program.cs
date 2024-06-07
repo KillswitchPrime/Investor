f8e45c23c2cf0aa1ab79df8c3c53fd9d9b17f9c2
KillswitchPrime <flip_d_@hotmail.com>
2024-06-07 23:19:15 +0200

    Program now functionally purchases a random stock from Alpaca API
    Only attached to paper API (no real money traded for now).

--- Program.cs
+++ Program.cs
@@ -1,2 +1,42 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Alpaca.Markets;
+using Investor;
+using Microsoft.Extensions.Configuration;
+
+//var paperUrl = "https://paper-api.alpaca.markets/v2";
+//var liveUrl = "https://api.alpaca.markets/v2";
+
+var secrets = new ConfigurationBuilder()
+    .AddJsonFile("appsettings.json")
+    .Build();
+
+var key = secrets.GetSection("Secrets").GetSection("ApiKey").Value ?? "";
+var secret = secrets.GetSection("Secrets").GetSection("ApiSecret").Value ?? "";
+var filePath = secrets.GetSection("Secrets").GetSection("FilePath").Value ?? "";
+
+
+var alpacaClient = Environments.Paper.GetAlpacaTradingClient(new SecretKey(key, secret));
+var random = new Random();
+
+
+var tradeExecuted = false;
+while (tradeExecuted is false)
+{
+    var ticker = TickerSelector.GetRandomTicker(random, filePath);
+
+    var result = await alpacaClient.PostOrderAsync(new NewOrderRequest(
+            ticker,
+            25,
+            OrderSide.Buy,
+            OrderType.Market,
+            TimeInForce.Day
+        )
+    );
+
+    if(result.OrderStatus is OrderStatus.Canceled || result.OrderStatus is OrderStatus.Rejected || result.OrderStatus is OrderStatus.Stopped)
+    {
+        continue;
+    }
+
+    tradeExecuted = true;
+    Console.WriteLine($"Created buy order for ticker: {ticker}, of quantity: 25.");
+}
---
+++ TickerSelector.cs
@@ -0,0 +1,19 @@
+﻿namespace Investor
+{
+    public static class TickerSelector
+    {
+        public static string GetRandomTicker(Random random, string filePath)
+        {
+            var fileData = File.ReadLines(filePath);
+            var tickers = new List<string>();
+            foreach (var line in fileData)
+            {
+                var items = line.Split(" ");
+                tickers.Add(items[0]);
+            }
+
+            var index = random.Next(tickers.Count);
+            return tickers[index];
+        }
+    }
+}
